6aba7ac21037662edd2d87c732769bc89a58fc1b
Winston Waters <Winstonw57@gmail.com>
2016-07-22 11:23:42 -0400

    routes to different views working

---
+++ js/controllers/reviews.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+  app.controller('ReviewsController', ['$scope', '$http', '$location', 'ReviewsService', function($scope, $http, $location, ReviewsService){
+
+    console.log('hihihihi users controller');
+
+  }])
+}
---
+++ js/services/reviews.js
@@ -0,0 +1,5 @@
+module.exports = function(app){
+  app.factory('ReviewsService', function($http){
+
+  })
+}
