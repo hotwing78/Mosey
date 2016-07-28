7685edbd32812ce08d627272287791d66f99d9d1
Winston Waters <Winstonw57@gmail.com>
2016-07-28 10:49:37 -0400

    reviews in works

--- public/js/app.js
+++ public/js/app.js
@@ -71,37 +71,39 @@ module.exports = function(app) {
 }
 
 },{}],3:[function(require,module,exports){
-module.exports = function(app){
-  app.controller('reviewsController', ['$scope', '$http', '$location', 'reviewsService', 'loginService', function($scope, $http, $location, reviewsService, loginService){
+module.exports = function(app) {
+    app.controller('reviewsController', ['$scope', '$http', '$location', 'reviewsService', 'loginService', function($scope, $http, $location, reviewsService, loginService) {
 
-    $scope.reviewList = reviewsService.getAllReviews();
-    $scope.username = loginService.getUsername();
-    $scope.errorMessage = '';
+        $scope.reviewList = reviewsService.getAllReviews();
+        $scope.username = loginService.getUsername();
+        $scope.errorMessage = '';
 
-    $scope.addReview = function(){
-      console.log(`send new review ${$scope.reviewText}`);
-      return $http({
-        method: 'POST',
-        url: '/reviews',
-        data: {
-          comment: $scope.reviewText,
-          username: 'teammosey'
-        }
-      }).then(function(response){
-        console.log('pina colada', response);
-        return reviewsService.getAllReviews();
-      }),function(response){
-        console.log('response', response.data.message);
-        $scope.errorMessage = response.data.message;
-      }
-    };
-
-    $scope.deleteReview= function(index){
-      $scope.reviewList.splice(index, 1);
-    };
+        $scope.addReview = function() {
+            console.log(`send new review ${$scope.reviewText}`);
+            return $http({
+                method: 'POST',
+                url: '/reviews',
+                data: {
+                    comment: $scope.reviewText
+                    // username: 'teammosey'
+                }
+            }).catch(function(response) {
+                console.log('response', response.data.message);
+                $scope.errorMessage = response.data.message;
+            }).then(function(response) {
+                console.log('pina colada', response);
+                return reviewsService.getAllReviews();
+            })
 
 
-  }])
+        };
+
+        $scope.deleteReview = function(index) {
+            $scope.reviewList.splice(index, 1);
+        };
+
+
+    }])
 }
 
 },{}],4:[function(require,module,exports){
