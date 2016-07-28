aef026744a82e670bfea93e6f7506eaf7039a763
hotwing78 <dlwingo@gmail.com>
2016-07-27 22:06:33 -0400

    Itinerary post completed

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -1,28 +1,30 @@
 module.exports = function(app) {
-    app.controller('mapController', ['$scope','$compile', 'Markers', function($scope,$compile, Markers) {
+    app.controller('mapController', ['$scope', '$compile', 'Markers', function($scope, $compile, Markers) {
 
-        $scope.random = function(){
-          console.log('clicked');
+        $scope.random = function() {
+          let point = Markers.getPoint();
+          Markers.intineraryAdd(point);
+            console.log('clicked');
+
         };
         Markers.getLocation();
-        Markers.getRestaurants().then(function(promise){
-          let food = promise;
-          for(let i = 0; i < food.length; i++){
-          Markers.setMarker(food[i],function(point) {
+        Markers.getRestaurants().then(function(promise) {
+            let food = promise;
+            for (let i = 0; i < food.length; i++) {
+                Markers.setMarker(food[i], function(point) {
 
-            var htmlElement =  '<div ng-click="random()" >'
-            + point.name + ' ADD' +'</div>'
-            var compiled = $compile(htmlElement)($scope)
-            return compiled[0];
+                        var htmlElement = `<div class = 'info'>
+                             Name:\t${point.name}</br>
+                             Price:\t${point.price}</br>
+                             Category:\t${point.category}</br>
+                             <button ng-click ="random()">ADD</button> </div>`
+                        var compiled = $compile(htmlElement)($scope)
+                            return compiled[0];
 
-          })
-        }
+                    }) //Passing in a anonomys function that returns compiled...On the fly.
+            }
         });
-          console.log('Log here');
 
-        //  Markers.setMarkers(Markers.getRestaurants().);
-        // scope.$watch('food', function() {
-        //     Markers.setMarker();
-        // }, true);
+
     }]);
 }
