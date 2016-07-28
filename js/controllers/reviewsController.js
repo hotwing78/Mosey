6596969c1d2c745df01927b0dccb3bdb9dfc924e
Winston Waters <Winstonw57@gmail.com>
2016-07-28 13:59:12 -0400

    message delete not yet working

--- js/controllers/reviewsController.js
+++ js/controllers/reviewsController.js
@@ -27,16 +27,24 @@ module.exports = function(app) {
 
         $scope.deleteReview = function(index) {
             console.log(index);
-
+            var comment = {
+              id: index.id,
+              comment: index.comment,
+              username: index.username
+            }
+            console.log(comment);
           return $http({
             method: 'DELETE',
             url: '/deletereviews',
-            data: index,
-          }).then(function(response){
-            console.log('deletttting this response: ', response);
-          }), function(error){
-            console.log('delete error');
-          }
+            data: comment,
+          }).then(function(res){
+            console.log(res);
+          })
+          // .then(function(response){
+          //   console.log('deletttting this response: ', response);
+          // }), function(error){
+          //   console.log('delete error');
+          // }
             $scope.reviewList.splice(index, 1);
         };
 
