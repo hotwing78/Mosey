f33e884975a43d5aa285ad270dc97c6e03e6849c
Damon Wingo <dlwingo@gmail.com>
2016-12-17 00:44:06 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('Mosey', ['ngRoute']);
 
 //controllers
-let log = require('./controllers/loginController.js')(app);
+require('./controllers/loginController.js')(app);
 require('./controllers/mapController.js')(app);
 require('./controllers/reviewsController.js')(app);
 require('./controllers/newSpotController.js')(app);
